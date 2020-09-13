8eee4391287ba5a3193461f0ee079da6ebb7fbf9
Brian Tweed <brian.tweed@avbrowne.com>
2020-09-13 21:34:57 +0100

    rename redux folder to storage

--- src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import {combineReducers} from "redux";
-import movieReducer from "./movieReducer";
-import movieSearchReducer from "./movieSearchReducer";
-
-
-export default combineReducers({
-    results: movieReducer,
-    search: movieSearchReducer
-});
