c6eb337f7ad14dd38175dc31bc0f897d2b4affec
Brian Tweed <brian.tweed@avbrowne.com>
2020-09-16 20:40:06 +0100

    throttle store subscribe

--- src/storage/store.js
+++ src/storage/store.js
@@ -1,19 +1,19 @@
 import {applyMiddleware, createStore} from "redux";
 import rootReducer from "./reducers";
 import thunk from "redux-thunk";
+import {throttle} from 'lodash';
 import {loadState, saveState} from "./localStorage";
 
 
 const loadedState = loadState();
 const store = createStore(rootReducer, loadedState, applyMiddleware(thunk));
 
-store.subscribe(() => {
-    saveState({
+store.subscribe(
+    throttle( () => saveState({
         search: store.getState().search,
         results: store.getState().results,
         history: store.getState().history
-    });
-});
-
+    }), 2000)
+);
 
 export default store;
