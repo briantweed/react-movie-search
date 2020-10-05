9b975fe801199aff68cdb575b2d92d62bdc58ae8
Brian Tweed <brian.tweed@avbrowne.com>
2020-10-05 08:56:33 +0100

    no message

---
+++ src/forms/myapimovies.js
@@ -0,0 +1,31 @@
+import {searchableYears} from "../helpers";
+
+export default {
+
+    name: "MyApiMovies",
+
+    fields: [
+
+        // MOVIE TITLE
+        {
+            "type": "text",
+            "name": "title",
+            "label": "Movie Title",
+            "class": "",
+            "validation": [
+                "required"
+            ]
+        },
+
+        // RELEASE YEAR
+        {
+            "type": "select",
+            "name": "year",
+            "label": "Release Year",
+            "options": searchableYears(),
+            "class": ""
+        }
+
+    ]
+
+};
